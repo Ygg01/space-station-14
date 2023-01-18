ac19b20ac1e1a9f2ad3d214c6ef2be845994fb41
Ygg01 <y.laughing.man.y@gmail.com>
2023-01-18 23:24:08 +0100

    Fix spill sound issue and minor warning fix

--- Content.Server/Chemistry/TileReactions/SpillIfPuddlePresentTileReaction.cs
+++ Content.Server/Chemistry/TileReactions/SpillIfPuddlePresentTileReaction.cs
@@ -6,20 +6,24 @@ using Content.Shared.FixedPoint;
 using JetBrains.Annotations;
 using Robust.Shared.Map;
 
-namespace Content.Server.Chemistry.TileReactions
-{
-    [UsedImplicitly]
-    [DataDefinition]
-    public sealed class SpillIfPuddlePresentTileReaction : ITileReaction
-    {
-        public FixedPoint2 TileReact(TileRef tile, ReagentPrototype reagent, FixedPoint2 reactVolume)
-        {
-            var spillSystem = EntitySystem.Get<SpillableSystem>();
-            if (reactVolume < 5 || !spillSystem.TryGetPuddle(tile, out _)) return FixedPoint2.Zero;
+namespace Content.Server.Chemistry.TileReactions;
 
-            return spillSystem.SpillAt(tile,new Solution(reagent.ID, reactVolume), "PuddleSmear", true, false, true) != null
-                ? reactVolume
-                : FixedPoint2.Zero;
+[UsedImplicitly]
+[DataDefinition]
+public sealed class SpillIfPuddlePresentTileReaction : ITileReaction
+{
+
+    public FixedPoint2 TileReact(TileRef tile, ReagentPrototype reagent, FixedPoint2 reactVolume)
+    {
+        var spillableSystem = IoCManager.Resolve<IEntityManager>().System<SpillableSystem>();
+
+        if (reactVolume < 5 || !spillableSystem.TryGetPuddle(tile, out _))
+        {
+            return FixedPoint2.Zero;
         }
+
+        return spillableSystem.SpillAt(tile, new Solution(reagent.ID, reactVolume), "PuddleSmear", noTileReact: true) != null
+            ? reactVolume
+            : FixedPoint2.Zero;
     }
 }
